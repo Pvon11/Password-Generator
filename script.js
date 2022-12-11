1fb487eb3c1fe04fe19e20473ae562066b2ff0b0
Pvon11 <patrickvonatbone@gmail.com>
2022-12-11 16:11:47 -0500

    Working but needs some debugging

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
 // Assignment code here
+var characterLength = 8;
+var choiceArr = [];
+
+var specialCharArr = ['!', '@','#', '$', '%', '^', '&', '*', '(', ')', '[', ']', '{', '}', '|', '?', '/', '<', '>'];
+var lowerCaseArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] 
+var upperCaseArr = ['A', 'B', 'C', "D", "E", 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+var numberArr = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// Write password to the #password input
+
+function writePassword() {
+  var correctPrompts = getPrompts(); // either T or F
+  var passwordText = document.querySelector("#password");
+
+  if(correctPrompts) {
+  
+  var newPassword = generatePassword();
+  passwordText.value = newPassword;
+  } else {
+    passwordText.value = "";
+  }
+}
+
+
 function generatePassword(length) {
   console.log("You clicked the button!")
-
-
-  // Prompt the user for password criteria
-  // password l
-
-  return " generated password";
+  var password = "";
+  for(var i = 0; i < characterLength; i++) {
+    var randomIndex = Math.floor(Math.random() * choiceArr.length);
+    password = password + choiceArr[randomIndex];
+  } 
+  return password;
 }
 
 function getPrompts() {
@@ -31,24 +61,3 @@ function getPrompts() {
     choiceArr = choiceArr.concat(numberArr);
   } return true;
 }
-
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-
-function writePassword() 
-  var correctPrompts = getPrompts(); // either T or F
-
-  if(correctPrompts) {
-  
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
